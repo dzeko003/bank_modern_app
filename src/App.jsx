e11774ed0dec86f5242d6f1f50bec58825f493d0
dzeko003 <berenismassamba@gmail.com>
2025-04-16 00:17:42 +0100

    add cardDeal section

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,24 @@ const App = () => (
 
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>NavBar</div>
+        <div className={`${styles.boxWidth}`}><NavBar/></div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}><Hero/></div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Sats</div>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+          </div>
       </div>
     </div>
 
